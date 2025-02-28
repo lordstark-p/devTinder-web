e405c2e33a70bc0d344ad7c790da7cb80fcadd94
lordstark-p <pranjalchandel98@gmail.com>
2025-02-28 18:48:38 +0530

    installed DaisyUi & added navbar components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  css: {
+    postcss: "./postcss.config.js", // Path to your PostCSS config
+  },
+});
