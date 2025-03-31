c1a59cef549a16da5f6c0cd466945246ddd946ee
lordstark-p <pranjalchandel98@gmail.com>
2025-03-31 20:10:10 +0530

    redirect to login if not authenticated

--- README.md
+++ README.md
@@ -20,3 +20,7 @@
 - Login & seee if your data is coming properly in the store
 - NavBar should update as soon as user logs in
 - Refactor our code to add constants file + create a components folder
+- You should not be able to access other routes without login
+- If token is not present, redirect user to login page
+- Logout
+- Profile Page
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
